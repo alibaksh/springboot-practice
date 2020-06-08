c38183ba97ebd92270ae985be4bed297fda2d833
Ali Mehmood <ali.mehmood@tenpearls.com>
2020-06-08 18:02:59 +0500

    Added support for internationalization

--- src/main/java/com/springbootpractice/restservices/hello/HelloWorldController.java
+++ src/main/java/com/springbootpractice/restservices/hello/HelloWorldController.java
@@ -1,21 +1,38 @@
 package com.springbootpractice.restservices.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-//	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+
 	@GetMapping(path = "/helloworld")
 	public String helloWorld() {
 		return "Hello World of Spring boot";
 	}
-	
+
 	@GetMapping(path = "/helloworld-bean")
 	public UserDetailsBean helloWorldBean() {
 		return new UserDetailsBean("Ali", "Mehmood", "Karachi-PK");
+	}
+
+	@GetMapping(path = "/helloworld-i18")
+	public String getMessagesIni18Format(
+			@RequestHeader(name = "Accept-Language", required = false, defaultValue = "en") String locale) {
+		return messageSource.getMessage("label.hello", null, new Locale(locale));
+	}
+
+	@GetMapping(path = "/helloworld-i182")
+	public String getMessagesIni18Format2() {
+		return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
 	}
 }
